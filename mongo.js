9e653b26f80d11ea8f21995a322d3e9f2a642d29
NikitaBoro <133001359+NikitaBoro@users.noreply.github.com>
2024-12-16 13:05:38 +0200

    Add files via upload

---
+++ mongo.js
@@ -0,0 +1,37 @@
+
+
+if (process.argv.length<3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+if (process.argv.length>5) {
+    console.log("To many arguments")
+    process.exit(1)
+}
+
+
+const Person = mongoose.model('Person',personSchema)
+
+if(process.argv.length===3){
+    Person.find({}).then(result => {
+        console.log("Phonebook:")
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+}
+
+if (process.argv.length===5) {
+
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4]
+    })
+    person.save().then(result=>{
+        console.log(`added ${person.name} number ${person.number} to phonebook`)
+        mongoose.connection.close()
+    })
+}
+
